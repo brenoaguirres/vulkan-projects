88a07df4874f2b084d0bac1240345ac73b04761d
brenoaguirres <aguirressoftware@gmail.com>
2025-09-01 15:58:28 -0300

    Initial commit

---
+++ VulkanTest/ex1.cpp
@@ -0,0 +1,68 @@
+//#include <vulkan/vulkan.h> // vulkan header will work alone as well but won't show anything
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h> // used to create window and show gfx
+
+#include <iostream> // report and propagate errors
+#include <stdexcept> // report and propagate errors
+#include <cstdlib> // EXIT_SUCCESS, EXIT_FAILURE
+
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
+
+class HelloTriangleApplication1
+{
+
+public:
+	void run()
+	{
+		initWindow();
+		mainLoop();
+		cleanup();
+	}
+
+private:
+	GLFWwindow* window;
+
+	void initWindow()
+	{
+		glfwInit(); // init window library
+
+		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API); // no OpenGL context since we use Vulkan
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE); // disable resizing for now
+
+		window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr); // create and init window - params: width, height, title, monitor (for fullscreen), only for OpenGL context
+	}
+
+	void mainLoop()
+	{
+		while (!glfwWindowShouldClose(window))
+		{
+			glfwPollEvents();
+		}
+	}
+
+	void cleanup()
+	{
+		glfwDestroyWindow(window);
+
+		glfwTerminate();
+	}
+};
+
+int example1()
+{
+	HelloTriangleApplication1 app;
+
+	try
+	{
+		app.run();
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
+}
---
+++ VulkanTest/ex2.cpp
@@ -0,0 +1,134 @@
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+
+#include <vector>
+
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
+
+class HelloTriangleApplication2
+{
+
+public:
+	void run()
+	{
+		initWindow();
+		initVulkan();
+		mainLoop();
+		cleanup();
+	}
+
+private:
+
+	GLFWwindow* window;
+	VkInstance instance;
+
+	void initWindow()
+	{
+		glfwInit();
+
+		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+		window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+	}
+
+	void initVulkan()
+	{
+		createInstance(); // instance - connection between application and Vulkan library
+	}
+
+	void mainLoop()
+	{
+		while (!glfwWindowShouldClose(window))
+		{
+			glfwPollEvents();
+		}
+	}
+
+	void cleanup()
+	{
+		vkDestroyInstance(instance, nullptr);
+
+		glfwDestroyWindow(window);
+
+		glfwTerminate();
+	}
+
+	void createInstance()
+	{
+		// init struct with info about the application, optional
+		// provides info for the driver to optimize for our app
+
+		VkApplicationInfo appInfo{};
+		appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+		appInfo.pApplicationName = "Hello Triangle";
+		appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.pEngineName = "No Engine";
+		appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.apiVersion = VK_API_VERSION_1_0;
+
+		// init struct with info about the global extensions and validation layers, not optional
+		VkInstanceCreateInfo createInfo{};
+		createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+		createInfo.pApplicationInfo = &appInfo;
+
+		// global extensions
+		uint32_t glfwExtensionCount = 0;
+		const char** glfwExtensions;
+
+		glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+		createInfo.enabledExtensionCount = glfwExtensionCount;
+		createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+		// validation layers
+		createInfo.enabledLayerCount = 0;
+
+		if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) // pointer to struct with creation info, custom allocator usually nullptr, pointer to the instance
+		{
+			throw std::runtime_error("failed to create instance!");
+		}
+
+		PrintExtensionSupport();
+	}
+
+	void PrintExtensionSupport()
+	{
+		// checking for extension support
+		uint32_t extensionCount = 0;
+		vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr); // get the number of extensions
+
+		std::vector<VkExtensionProperties> extensions(extensionCount); // array to hold the extensions details
+
+		vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data()); // get the extensions - contains name and version
+
+		std::cout << "available extensions:\n";
+
+		for (const auto& extension : extensions)
+		{
+			std::cout << '\t' << extension.extensionName << '\n';
+		}
+	}
+};
+
+int exercise2()
+{
+	HelloTriangleApplication2 app;
+
+	try
+	{
+		app.run();
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
+}
